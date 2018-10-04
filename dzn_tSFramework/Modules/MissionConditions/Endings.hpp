721ac320337560bab1f222e39bbb814ef16e520d
Vocasyan <vocasyan@gmail.com>
2018-10-04 16:44:46 +0300

    1

--- dzn_tSFramework/Modules/MissionConditions/Endings.hpp
+++ dzn_tSFramework/Modules/MissionConditions/Endings.hpp
@@ -1,15 +1,21 @@
 class CfgDebriefing
-{  
-    class WIN
-    {
-        title = "Задание выполнено";
-        subtitle = "Успех!";
-        description = "Задача выполнена";
-    };                  
-    class WIPED
-    {
-        title = "Задание провалено";
-        subtitle = "Все погибли!";
-        description = "Такие дела...";
-    };
+{
+	class WIN
+	{
+		title = "Миссия выполнена";
+		subtitle = "Восхитительный успех!";
+		description = "Армия США успешно уничтожила ПВО";
+	};
+	class WIPED
+	{
+		title = "Миссия провалена";
+		subtitle = "Все погибли";
+		description = "Вы не успели уничтожить ПВО";
+	};
+	class Retreat
+	{
+		title = "Миссия провалена";
+		subtitle = "Отступаем!!!";
+		description = "Армия США отступила и не успела уничтожить ПВО";
+	};
 };
